afa56452fe6a8986f39638cc7e000ed4b1314977
Richard Leichner <richard.leichner@pwc.com>
2023-08-02 22:12:08 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'body-and-weight',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
